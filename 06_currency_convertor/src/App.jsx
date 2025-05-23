05ae7344cf44185a870c474ba684362c91ec678c
Diksha Bharti <bhartidiksha2008@gmail.com>
2025-05-23 23:28:16 +0530

    custom react

---
+++ 06_currency_convertor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {setCurrencyInfo} from './hooks/useCurrencyInfo'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl bg-orange-500">Currency App with Chai</h1>
+    </>
+  )
+}
+
+export default App
