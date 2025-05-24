88593a05210070c1c42106a8d65462d68338e0c2
Diksha Bharti <bhartidiksha2008@gmail.com>
2025-05-24 20:44:23 +0530

    project

---
+++ 07_router/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+/* <a> tag is not used in React as it refreshes the whole page so we use <link> tag */
+
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <h1 className="text-center bg-orange-500 text-lg">Diksha Bharti</h1> */ }
+    </>
+  )
+}
+
+export default App
